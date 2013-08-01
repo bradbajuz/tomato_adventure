7f27ddd50d6a0d076dd33fd2c02838467f7ba0ab
bradbajuz <Brad@brad-mba.local>
2013-08-01 19:34:30 -0400

    Start plum implementation

--- tomato_adventure.rb
+++ tomato_adventure.rb
@@ -3,7 +3,31 @@ def prompt()
 end
 
 def plum_tomato
-  
+  puts "Week 1: Do you water or wait?"
+  tomato_grow = false
+
+  while true
+    prompt; next_move = gets.chomp
+
+    if next_move == "wait"
+      dead("The seeds never sprout and no tomatos grow.")
+    elsif next_move == "water" and not tomato_grow
+      puts "Great! The seeds love the drink."
+      tomato_grow = true
+
+      puts "Week 4: Your tomatos are growing. Cage or no cage?"
+    elsif  next_move == "cage" and tomato_grow
+      puts "Great! Your tomatoes are 4 feet high."
+    elsif next_move == "no cage" and tomato_grow
+      puts "Your tomatoes a growing as a viny bush"
+
+
+
+      
+    else
+      puts "I don't understand your input."
+    end
+  end
 end
 
 def cherry_tomato
@@ -13,8 +37,6 @@ end
 def dead(why)
   puts "#{why} Good Job!"
   Process.exit(0)
-end
-  
 end
 
 def start()
